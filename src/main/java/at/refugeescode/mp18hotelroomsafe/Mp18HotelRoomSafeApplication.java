b3eeeba0f695854d79d53abc7b9c72169e833abd
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-23 18:05:56 +0200

    finished the first version of mp18

---
+++ src/main/java/at/refugeescode/mp18hotelroomsafe/Mp18HotelRoomSafeApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp18hotelroomsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp18HotelRoomSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp18HotelRoomSafeApplication.class, args);
+	}
+}
